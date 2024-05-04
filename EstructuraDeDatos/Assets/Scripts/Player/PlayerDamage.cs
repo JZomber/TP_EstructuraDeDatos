96f498a47603888550420957d0ce8076af041571
JZomber <103089013+JZomber@users.noreply.github.com>
2024-05-04 16:22:53 -0300

    Traps damage
    - El jugador puede recibir o morir por daño de trampas

--- EstructuraDeDatos/Assets/Scripts/Player/PlayerDamage.cs
+++ EstructuraDeDatos/Assets/Scripts/Player/PlayerDamage.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
@@ -15,6 +16,16 @@ public class PlayerDamage : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy") && canTakeDamage) // Si el jugador colisiona con un enemigo y puede tomar daño
+        {
+            lifeSystem.TakeDamage(1); // Reduce la vida del jugador en 1
+            canTakeDamage = false; // Deshabilita la capacidad de tomar daño temporalmente
+            StartCoroutine(ResetDamageCooldown()); // Espera un tiempo antes de permitir que el jugador tome daño nuevamente
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Trap") && canTakeDamage) //Si el jugador colisiona y/o se queda en la trampa, recibe daño
         {
             lifeSystem.TakeDamage(1); // Reduce la vida del jugador en 1
             canTakeDamage = false; // Deshabilita la capacidad de tomar daño temporalmente
