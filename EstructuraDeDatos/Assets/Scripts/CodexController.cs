7b731ba7d91c5db959c953eacbed65b10d1cbdda
Ifritdo <JulianFirpo316@hotmail.com>
2024-06-14 17:58:02 -0300

    Commit Tda
    Cree los scripts CodexController y BBA
    En la escena del codex fui metiendo cosas para la UI (Botones, Texto, paneles, etc) pero esta como todo suelto tengo que ordenarlo

---
+++ EstructuraDeDatos/Assets/Scripts/CodexController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CodexController : MonoBehaviour
+{
+    public Transform contentPanel;
+    public GameObject enemyCardPrefab;
+    public Button sortHealthAscButton;
+    public Button sortHealthDescButton;
+    public Button sortNameButton;
+
+    private ABB arbol;
+
+    void Start()
+    {
+        arbol = new ABB();
+        EnemyScript[] enemies = FindObjectsOfType<EnemyScript>();
+
+        foreach (var enemy in enemies)
+        {
+            arbol.AgregarElem(enemy);
+        }
+
+        sortHealthAscButton.onClick.AddListener(SortByHealthAsc);
+        sortHealthDescButton.onClick.AddListener(SortByHealthDesc);
+        sortNameButton.onClick.AddListener(SortByName);
+
+        SortByHealthAsc(); // Ordenar por defecto al iniciar
+    }
+
+    void SortByHealthAsc()
+    {
+        ClearContent();
+        arbol.RecorrerEnOrdenAscendente(AddEnemyCard);
+    }
+
+    void SortByHealthDesc()
+    {
+        ClearContent();
+        arbol.RecorrerEnOrdenDescendente(AddEnemyCard);
+    }
+
+    void SortByName()
+    {
+        ClearContent();
+        arbol.RecorrerPorNombre(AddEnemyCard);
+    }
+
+    void AddEnemyCard(EnemyScript enemy)
+    {
+        GameObject newCard = Instantiate(enemyCardPrefab, contentPanel);
+
+        // Asignar valores directamente en los componentes de la carta
+        TextMeshProUGUI[] texts = newCard.GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (var text in texts)
+        {
+            if (text.name == "NameText")
+            {
+                text.text = enemy.name;
+            }
+            else if (text.name == "HealthText")
+            {
+                text.text = enemy.health.ToString();
+            }
+        }
+
+        Image[] images = newCard.GetComponentsInChildren<Image>();
+        foreach (var image in images)
+        {
+            if (image.name == "EnemyImage")
+            {
+                // Asigna la imagen del enemigo aquí si tienes una
+                // image.sprite = enemy.sprite;
+            }
+        }
+    }
+
+    void ClearContent()
+    {
+        foreach (Transform child in contentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
